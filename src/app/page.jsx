946efc5f06490742f5fd0ac47c9f514827abe1f4
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-06-20 16:34:58 +0530

    feat: implement HeroSection component with scroll effects and gradient styling

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import HeroSection from '@/components/hero-section'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <div className='grid-background'></div>
+
+       <HeroSection />
+    </div>
+  )
+}
