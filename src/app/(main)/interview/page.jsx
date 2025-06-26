554427ad390fc0caa174b55883f31a3c227cbef2
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-06-26 14:44:24 +0530

    feat: implement mock interview page with navigation, quiz component, and performance tracking; enhance interview preparation page with assessment stats and charts

--- src/app/(main)/interview/page.jsx
+++ src/app/(main)/interview/page.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { getAssessments } from "../../../../actions/interview";
+import PerformanceChart from "./_components/performance-charts";
+import QuizList from "./_components/quiz-lists";
+import StatsCards from "./_components/stats-cards";
 
-export default function InterviewPgae() {
+
+export default async function InterviewPrepPage() {
+  const assessments = await getAssessments();
+
   return (
-    <div>InterviewPgae</div>
-  )
+    <div>
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="text-6xl font-bold gradient-title">
+          Interview Preparation
+        </h1>
+      </div>
+      <div className="space-y-6">
+        <StatsCards assessments={assessments} />
+        <PerformanceChart assessments={assessments} />
+        <QuizList assessments={assessments} />
+      </div>
+    </div>
+  );
 }
