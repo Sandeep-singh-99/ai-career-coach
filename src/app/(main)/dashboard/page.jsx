c2a558fb7d51555a0bddfcb763b12c2dc350e9fb
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-11-06 16:20:29 +0530

    style: Refactor formatting and spacing in IndustryInsightsPage component for consistency

--- src/app/(main)/dashboard/page.jsx
+++ src/app/(main)/dashboard/page.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { fetchUserOnBoardingData } from '../../../../actions/user'
-import { redirect } from 'next/navigation'
-import { getIndustryInsights } from '../../../../actions/dashboard'
-import DashboardView from './_components/dashboard-view'
-
+import React from "react";
+import { fetchUserOnBoardingData } from "../../../../actions/user";
+import { redirect } from "next/navigation";
+import { getIndustryInsights } from "../../../../actions/dashboard";
+import DashboardView from "./_components/dashboard-view";
 
 export default async function IndustryInsightsPage() {
-     const {isOnboarded } = await fetchUserOnBoardingData()
-     const insights = await getIndustryInsights()
-    
-      if (!isOnboarded) {
-        redirect('/onboarding')
-      }
+  const { isOnboarded } = await fetchUserOnBoardingData();
+
+  if (!isOnboarded) {
+    redirect("/onboarding");
+  }
+
+  const insights = await getIndustryInsights();
   return (
-    <div className='container mx-auto'>
+    <div className="container mx-auto">
       <DashboardView insights={insights} />
     </div>
-  )
+  );
 }
